9238f0498821434f4c8a0ded7689ba09b83b170d
mwagoh <useremail>
2024-04-29 10:08:13 +0300

    files added

---
+++ app.js
@@ -0,0 +1,40 @@
+//Create a function that retruns th lenght of the array
+
+//let numberArray = [123,45,6,90,0,10000]
+
+//console.log(numberArray.length);
+
+//create a function that returns the sum of numbers in an array
+
+// let sumArray = [3,3,5,5]
+
+// let total = 0;
+
+// for (let i = 0; i < sumArray.length; i++) {
+//     total += sumArray[i];
+    
+// }
+//console.log(total);
+
+// sumArray.forEach((i) => total +=i )
+
+// console.log(total);
+
+//Create a function that checks if the name Onesmus exists in a given array
+
+// let namesArray = ["Justus", "Godfrey", "Onesmus", "Boniface"]
+
+// console.log(namesArray.includes("Onesmus"));
+
+
+//Create a function that return even numbers in a given array.
+
+let intArrays = [1,2,3,4,4,55,6,7,8]
+
+let even = []
+intArrays.forEach((i) => {
+    if (i%2 = 0)
+    even.push(i)
+})
+console.log(even numbers in array are: $[even]);
+
